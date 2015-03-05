bc44e260af4d83121eb95c342aaa639c690386b9
jijing <goco.v@163.com>
2015-03-05 17:36:07 +0800

    Update t1.py

--- t1.py
+++ t1.py
@@ -11,3 +11,19 @@ from pwd import getpwuid
 def getown(path):
     return getpwuid(stat(path).st_uid).pw_name
     
+import uuid 
+path = path_list[0]
+
+def createjobs(path_list):
+    for path in path_list:
+        slug = uuid.uuid3(uuid.NAMESPACE_DNS,path)
+        job1 = Jobs(path=path,slug=slug)
+        job1.save()
+        
+name  = "dgital-regression"
+slug = uuid.uuid3(uuid.NAMESPACE_DNS,name)
+jobs = Jobs.objects.filter(paht__endswith="tsim")
+g1 = Group.objects.filter(name=name)[0]
+for i in jobs:
+    g1.jobs.add(i)
+    
