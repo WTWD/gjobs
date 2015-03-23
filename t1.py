b8f92cf8d4a366231d83f41c3eea7caaf0ad12cf
jijing <goco.v@163.com>
2015-03-23 14:57:40 +0800

    Update t1.py

--- t1.py
+++ t1.py
@@ -5,6 +5,20 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE","ldj17.settings")
 django.setup()
 
+#  from __future__ import absolute_import
+#  from django.core.management import setup_environ
+#  import sys,os
+#  sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) 
+#  try:
+#      import settings # Assumed to be in the same directory.
+#  except ImportError:
+#      sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
+#      sys.exit(1)
+#   
+#  project_directory = setup_environ(settings)
+#  project_name = os.path.basename(project_directory)
+#  os.environ['DJANGO_SETTINGS_MODULE'] = "%s.settings" % project_name  
+
 # from os import stat
 # from pwd import getpwuid
 
